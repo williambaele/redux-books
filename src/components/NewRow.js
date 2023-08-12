3e5cafa7aec77a6f8aa111d3b1792a5113e061d0
William Baele <baelewilliam1999@gmail.com>
2023-08-12 11:32:50 +0200

    better UI on new row

--- src/components/NewRow.js
+++ src/components/NewRow.js
@@ -24,25 +24,25 @@ const NewRow = () => {
 
       <td class="h-px w-px whitespace-nowrap">
         <div class="px-6 py-3">
-          <span class="inline-flex items-center gap-1.5 py-0.5 px-2 rounded-full text-xs font-medium bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200">
+          <span class="inline-flex items-center gap-1.5 py-0.5 px-2 rounded-full text-xs font-medium bg-gray-300 text-gray-800">
             <svg
-              class="w-2.5 h-2.5"
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
+              class="bi bi-hourglass-split"
               viewBox="0 0 16 16"
             >
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
             </svg>
-            Successful
+            Pending
           </span>
         </div>
       </td>
       <td class="h-px w-px whitespace-nowrap">
         <div class="px-6 py-3">
           <span class="text-sm text-gray-600 py-1 rounded-md outline-none">
-            28 Dec, 12:12
+            Today
           </span>
         </div>
       </td>
