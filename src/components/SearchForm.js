28bd42a2897a8f5d1d2a232fb5749e7cb3fcf25b
William Baele <williambaele@Air-de-William.lan>
2023-08-14 12:02:31 +0200

    added search component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,74 +2,35 @@ import React from "react";
 
 const SeachForm = () => {
   return (
-    <div className="w-full h-full p-6">
-      <div className="w-full h-full">
-        <div class="flex flex-col">
-          <div class="-m-1.5 overflow-x-auto">
-            <div class="p-1.5 w-full inline-block align-middle">
-              <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
-                <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700">
-                  <div>
-                    <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200">
-                      Your personnal library
-                    </h2>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">
-                      Manage all your favorite books in a minute.
-                    </p>
-                  </div>
-
-                  <div>
-                    <div class="inline-flex gap-x-2">
-                      <div class="cursor-pointer py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
-                        <svg
-                          class="w-3 h-3"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          viewBox="0 0 16 16"
-                          fill="none"
-                        >
-                          <path
-                            d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                          />
-                        </svg>
-                        Create
-                      </div>
-                    </div>
-                  </div>
+    <div className="w-full h-full">
+      <div class="flex flex-col">
+        <div class="-m-1.5 overflow-x-auto">
+          <div class="p-1.5 w-full inline-block align-middle">
+            <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
+              <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700">
+                <div>
+                  <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                    Search your next book
+                  </h2>
+                  <p class="text-sm text-gray-600 dark:text-gray-400">
+                    Search and find you next gem to read !
+                  </p>
                 </div>
-
-                <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-gray-700">
-                  <div>
-                    <p class="text-sm text-gray-600 dark:text-gray-400">
-                      <span class="font-semibold text-gray-800 dark:text-gray-200">
-                        6
-                      </span>{" "}
-                      results
-                    </p>
-                  </div>
-
-                  <div>
-                    <button
-                      type="button"
-                      class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm hover:bg-red-500 hover:text-white"
-                    >
+                <div>
+                  <div class="inline-flex gap-x-2">
+                    <div class="cursor-pointer py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
                         height="16"
                         fill="currentColor"
-                        class="bi bi-trash"
+                        class="bi bi-search"
                         viewBox="0 0 16 16"
                       >
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                       </svg>
-                      Delete all books
-                    </button>
+                      Search
+                    </div>
                   </div>
                 </div>
               </div>
